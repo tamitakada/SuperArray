c0897b7cb0192cb04b5f2114815c16f7826262a0
Tami Takada <tami@mytalos.com>
2020-11-10 10:46:08 -0500

    added tests for get exceptions

--- SuperTester.java
+++ SuperTester.java
@@ -18,6 +18,7 @@ public class SuperTester {
     System.out.println(testToArray());
     System.out.println(testLastIndex());
     System.out.println(Arrays.toString(testConstructorExceptions()));
+    System.out.println(Arrays.toString(testGetExceptions()));
   }
 
   public static boolean testArrayCreation() {
@@ -362,6 +363,68 @@ public class SuperTester {
         testRecord[3 + i] = result;
       } catch (IllegalArgumentException e) {
         testRecord[3 + i] = !result;
+      }
+    }
+
+    return testRecord;
+  }
+
+  public static boolean[] testGetExceptions() {
+    boolean[] testRecord = new boolean[14];
+
+    String[] oneData = new String[] {
+      "1", "two", " 3 3", "", "1", "Six"
+    };
+    SuperArray one = addData(oneData);
+
+    try {
+      one.get(12);
+      testRecord[0] = false;
+    } catch (IndexOutOfBoundsException e) {
+      testRecord[0] = true;
+    }
+
+    try {
+      one.get(2);
+      testRecord[1] = true;
+    } catch (IndexOutOfBoundsException e) {
+      testRecord[1] = false;
+    }
+
+    try {
+      one.get(0);
+      testRecord[2] = true;
+    } catch (IndexOutOfBoundsException e) {
+      testRecord[2] = false;
+    }
+
+    try {
+      one.get(-33);
+      testRecord[3] = false;
+    } catch (IndexOutOfBoundsException e) {
+      testRecord[3] = true;
+    }
+
+    for (int i = 0; i < 10; i++) {
+      Random rng = new Random();
+      String[] testData = createRandomArr();
+      SuperArray test = addData(testData);
+
+      int arg = rng.nextInt(test.size() * 2);
+      int changeSign = rng.nextInt(2);
+
+      if (changeSign == 0) {
+        arg *= -1;
+      }
+
+      boolean result = true;
+      if ((arg < 0) || (arg >= test.size())) result = false;
+
+      try {
+        test.get(arg);
+        testRecord[4 + i] = result;
+      } catch (IndexOutOfBoundsException e) {
+        testRecord[4 + i] = !result;
       }
     }
 
