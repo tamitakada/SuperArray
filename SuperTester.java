fc6bff2977bd0273d37cf931800bbe6181cf8922
Tami Takada <tami@mytalos.com>
2020-11-10 10:52:19 -0500

    added tests for add exceptions

--- SuperTester.java
+++ SuperTester.java
@@ -20,6 +20,7 @@ public class SuperTester {
     System.out.println(Arrays.toString(testConstructorExceptions()));
     System.out.println(Arrays.toString(testGetExceptions()));
     System.out.println(Arrays.toString(testSetExceptions()));
+    System.out.println(Arrays.toString(testAddExceptions()));
   }
 
   public static boolean testArrayCreation() {
@@ -485,6 +486,68 @@ public class SuperTester {
 
       try {
         test.set(arg, "test");
+        testRecord[4 + i] = result;
+      } catch (IndexOutOfBoundsException e) {
+        testRecord[4 + i] = !result;
+      }
+    }
+
+    return testRecord;
+  }
+
+  public static boolean[] testAddExceptions() {
+    boolean[] testRecord = new boolean[14];
+
+    String[] oneData = new String[] {
+      "1", "two", " 3 3", "", "1", "Six"
+    };
+    SuperArray one = addData(oneData);
+
+    try {
+      one.add(12, "hello");
+      testRecord[0] = false;
+    } catch (IndexOutOfBoundsException e) {
+      testRecord[0] = true;
+    }
+
+    try {
+      one.add(2, "hello");
+      testRecord[1] = true;
+    } catch (IndexOutOfBoundsException e) {
+      testRecord[1] = false;
+    }
+
+    try {
+      one.add(6, "hello");
+      testRecord[2] = true;
+    } catch (IndexOutOfBoundsException e) {
+      testRecord[2] = false;
+    }
+
+    try {
+      one.add(-33, "hello");
+      testRecord[3] = false;
+    } catch (IndexOutOfBoundsException e) {
+      testRecord[3] = true;
+    }
+
+    for (int i = 0; i < 10; i++) {
+      Random rng = new Random();
+      String[] testData = createRandomArr();
+      SuperArray test = addData(testData);
+
+      int arg = rng.nextInt(test.size() * 2);
+      int changeSign = rng.nextInt(2);
+
+      if (changeSign == 0) {
+        arg *= -1;
+      }
+
+      boolean result = true;
+      if ((arg < 0) || (arg > test.size())) result = false;
+
+      try {
+        test.add(arg, "test");
         testRecord[4 + i] = result;
       } catch (IndexOutOfBoundsException e) {
         testRecord[4 + i] = !result;
